24d1ac75d810ba6fe8573944d02385f470ae97b2
Michelle <75897801+MichelleMiGG@users.noreply.github.com>
2021-02-25 12:36:16 -0300

    Create troca_valor.cpp

---
+++ troca_valor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void troca_valor(int a,int b){
+int temp;
+temp = a;
+a = b;
+b = temp;
+}
+
+int main() {
+int a = 2, b = 3;
+cout << "Antes a =  " << a << " = b " << b << endl;
+troca_valor(a,b);
+cout << "Depois a = " << a << " = b " << b << endl;
+return 0;
+}
